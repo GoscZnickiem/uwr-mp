20553a90613d63437db34f998b499bf5ddc43520
GoscZnickiem <maciek.6098@gmail.com>
2024-06-02 13:20:48 +0200

    a

--- p3/_build/default/src/compiler.ml
+++ p3/_build/default/src/compiler.ml
@@ -15,17 +15,18 @@ let get_var_addr (v : var) (st : stack) : cmd list =
         | None -> [LOAD (s - 1)] @ s_rec v st
     in [TOP] @ s_rec v st
 
-let sepc = ':' let sep = String.make 1 sepc
+let sepc = ':' 
+let sep = String.make 1 sepc
 
-let full_fun_name (f : name) (callst : name list) : name = String.concat sep (callst @ [f])
+let full_fun_name (f : name) (ancest : name list) : name = String.concat sep (ancest @ [f])
 
-let get_fun (f : name) (callst : name list) (fs : name list) : name * int = 
+let get_fun (f : name) (hierarchy : name list) (fs : name list) : name * int = 
     let rec names lst = 
         match lst with
         | _ :: tl -> full_fun_name f lst :: names tl
         | [] -> [f]
-    in let res = List.find (fun n -> List.exists (fun x -> n = x) fs) (names callst)
-    in res, List.length callst - List.length (String.split_on_char sepc res) + 1
+    in let res = List.find (fun n -> List.exists (fun x -> n = x) fs) (names hierarchy)
+    in res, List.length hierarchy - List.length (String.split_on_char sepc res) + 1
 
 let push_sframe (fspec : int) (st : stack) : cmd list = 
     let rec p_rec i st = 
@@ -33,52 +34,51 @@ let push_sframe (fspec : int) (st : stack) : cmd list =
         in if i = 0 then [LEA (s - List.length fr)] else [LOAD (s - 1)] @ p_rec (i - 1) st
     in [TOP] @ p_rec fspec st @ [PUSH]
 
-let rec compile_aexp (e : aexp) (st : stack) (callst : name list) (fs : name list) : cmd list =
+let rec compile_aexp (e : aexp) (st : stack) (hierarchy : name list) (fs : name list) : cmd list =
     match e with
     | Int n -> [CONST n]
     | Var v -> get_var_addr v st @ [LOAD 0]
-    | Binop(op, e1, e2) -> compile_aexp e1 st callst fs @ [PUSH] @ compile_aexp e2 (append st 1) callst fs @ [PRIM op]
+    | Binop(op, e1, e2) -> compile_aexp e1 st hierarchy fs @ [PUSH] @ compile_aexp e2 (append st 1) hierarchy fs @ [PRIM op]
     | Call (f, args) -> 
         let rec push_args args st = 
             match args with
-            | a :: ax -> compile_aexp a st callst fs @ [PUSH] @ push_args ax (append st 1)
+            | a :: ax -> compile_aexp a st hierarchy fs @ [PUSH] @ push_args ax (append st 1)
             | [] -> []
-        in let (fname, fspec) = get_fun f callst fs
+        in let (fname, fspec) = get_fun f hierarchy fs
         in push_sframe fspec st @ push_args args (append st 1) @ [CALL fname] @ [LEAVE (List.length args + 1)]
 
-let rec compile_bexp (e : bexp) (st : stack) (callst : name list) (fs : name list) : cmd list =
+let rec compile_bexp (e : bexp) (st : stack) (hierarchy : name list) (fs : name list) : cmd list =
     match e with
     | Bool b -> if b then [CONST 1] else [CONST 0]
-    | Cmp(op, e1, e2) -> compile_aexp e1 st callst fs @ [PUSH] @ compile_aexp e2 (append st 1) callst fs @ [CMP op]
-    | And(e1, e2) -> compile_bexp e1 st callst fs @ [BRANCH( compile_bexp e2 st callst fs, [CONST 0] )]
-    | Or(e1, e2) -> compile_bexp e1 st callst fs @ [BRANCH( [CONST 1], compile_bexp e2 st callst fs )]
-    | Not e -> compile_bexp e st callst fs @ [BRANCH( [CONST 0], [CONST 1] )]
+    | Cmp(op, e1, e2) -> compile_aexp e1 st hierarchy fs @ [PUSH] @ compile_aexp e2 (append st 1) hierarchy fs @ [CMP op]
+    | And(e1, e2) -> compile_bexp e1 st hierarchy fs @ [BRANCH( compile_bexp e2 st hierarchy fs, [CONST 0] )]
+    | Or(e1, e2) -> compile_bexp e1 st hierarchy fs @ [BRANCH( [CONST 1], compile_bexp e2 st hierarchy fs )]
+    | Not e -> compile_bexp e st hierarchy fs @ [BRANCH( [CONST 0], [CONST 1] )]
 
-let rec compile_stmt (s : stmt) (st : stack) (callst : name list) (fs : name list) : cmd list = 
+let rec compile_stmt (s : stmt) (st : stack) (varc : int) (hierarchy : name list) (fs : name list) : cmd list = 
     match s with
-    | Block ss -> List.concat_map (fun s -> compile_stmt s st callst fs) ss
-    | Assgn (v, e) -> get_var_addr v st @ [PUSH] @ compile_aexp e (append st 1) callst fs @ [STORE 0]
-    | If (c, e1, e2) -> compile_bexp c st callst fs @ [BRANCH( compile_stmt e1 st callst fs, compile_stmt e2 st callst fs )]
-    | While (c, e) -> [WHILE( compile_bexp c st callst fs, compile_stmt e st callst fs )]
+    | Block ss -> List.concat_map (fun s -> compile_stmt s st varc hierarchy fs) ss
+    | Assgn (v, e) -> get_var_addr v st @ [PUSH] @ compile_aexp e (append st 1) hierarchy fs @ [STORE 0]
+    | If (c, e1, e2) -> compile_bexp c st hierarchy fs @ [BRANCH( compile_stmt e1 st varc hierarchy fs, compile_stmt e2 st varc hierarchy fs )]
+    | While (c, e) -> [WHILE( compile_bexp c st hierarchy fs, compile_stmt e st varc hierarchy fs )]
     | Read v -> get_var_addr v st @ [PUSH; READ; STORE 0]
-    | Write e -> compile_aexp e st callst fs @ [WRITE]
-    | Return e -> compile_aexp e st callst fs
+    | Write e -> compile_aexp e st hierarchy fs @ [WRITE]
+    | Return e -> compile_aexp e st hierarchy fs @ [LEAVE varc; RET]
 
-let compile_fun (name : name) (vars : var list) (code : stmt) (callst : name list) (fs : name list) (stack : stack) : name * cmd list =
-    let varc = List.length vars
-    in full_fun_name name callst, [ENTER varc] @ (compile_stmt code stack (name :: callst) fs) @ [LEAVE varc; RET]
+let compile_fun (name : name) (varc : int) (code : stmt) (ancest : name list) (fs : name list) (stack : stack) : name * cmd list =
+    full_fun_name name ancest, [ENTER varc] @ (compile_stmt code stack varc (name :: ancest) fs)
 
-let rec compile_funcs (funcs : func list) (callst : name list) (fs : name list) (stack : stack) : (name * cmd list) list =
+let rec compile_funcs (funcs : func list) (ancest : name list) (fs : name list) (stack : stack) : (name * cmd list) list =
     List.concat_map (fun (Func(name, args, vars, localf, code)) -> 
-        let newstack = List.fold_left alloc (emptyframe :: stack) ([":stack#frame#addr:"] @ args @ [":ret#adrr:"] @ vars)
-        in compile_fun name vars code callst fs newstack :: compile_funcs localf (name :: callst) fs newstack
+        let newstack = List.fold_left alloc (emptyframe :: stack) ([":frame#addr:"] @ args @ [":ret#adrr:"] @ vars)
+        in compile_fun name (List.length vars) code ancest fs newstack :: compile_funcs localf (name :: ancest) fs newstack
     ) funcs
 
 let rec gen_fun_names (funcs : func list) (prefix : string) : name list = 
     List.concat_map (fun (Func(name, _, _, fs, _)) -> (prefix ^ name) :: gen_fun_names fs (prefix ^ name ^ sep)) funcs
 
-let compile_prog ((vars, funcs, main) : prog) : vm_prog = 
+let compile_prog ((vars, funcs, code) : prog) : vm_prog = 
     let stack = List.fold_left alloc [emptyframe] vars
     and varc = List.length vars
     and fnames = gen_fun_names funcs ""
-    in [ENTER varc] @ (compile_stmt main stack [] fnames) @ [LEAVE varc], compile_funcs funcs [] fnames stack
+    in [ENTER varc] @ (compile_stmt code stack 0 [] fnames) @ [LEAVE varc], compile_funcs funcs [] fnames stack
